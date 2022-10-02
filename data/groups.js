41904fc33055c88d3d7c02ed05b7dfb74473ce18
kdraganik <karol.draganik@gmail.com>
2022-10-02 08:00:27 +0200

    Data update 02.10.22

--- data/groups.js
+++ data/groups.js
@@ -6,10 +6,28 @@ const groups = [
     description: "Są to spotkania, podczas których rozmawiamy o sprawach ważnych dla kobiet, wspieramy się nawzajem, modlimy się o swoje potrzeby. Spotkania odbywają się w sali Kierunku. Prowadzone są w formie dyskusji i otwartej rozmowy przy kawie i ciastkach. "
   },
   {
+    name: "Grupa u Klaudii i Michała",
+    time: "wt 19:00",
+    place: "ul. Litewska (Psie Pole)",
+    description: "Miejsce, gdzie w zabieganej codzienności możesz się bezpiecznie zatrzymać i spędzić czas w rodzinnej atmosferze. Przy dobrym jedzonku dzielimy się tym, co przeżywamy, wzajemnie zachęcamy i spędzamy czas na wspólnej modlitwie. Jeśli masz pragnienie uczyć się praktycznych rzeczy w życiu z Bogiem na podstawie Słowa Bożego, ta grupa jest dla Ciebie!"
+  },
+  {
+    name: "Grupa studencka",
+    time: "wt 19:30",
+    place: "ul. Cybulskiego (Śródmieście)",
+    description: "Wrocław to miasto pełne studentów, a my chcemy ich lepiej poznać! Spotykamy się każdego tygodnia, żeby wspólnie odpocząć, pomodlić się i porozmawiać o ważnych tematach. Dołącz do nas i nie przegap środowych wieczorów, bo razem z Tobą i wieloma wspaniałymi ludźmi chcemy uczyć się, co to znaczy żyć z Bogiem każdego dnia."
+  },
+  {
     name: "Grupa u Martyny i Mateusza",
-    time: "śr 18:30",
+    time: "śr 18:00",
     place: "ul. Vespucciego (Swojczyce)",
     description: "Podczas naszych spotkań modlimy się o siebie nawzajem, studiujemy historie biblijne i dyskutujemy nad bieżącymi wydarzeniami w Polsce i na świecie. Towarzyszy nam miła atmosfera i smaczny poczęstunek. "
+  },
+  {
+    name: "Spotkania modlitewne",
+    time: "śr 18:00",
+    place: "Kościół kieunek",
+    description: "Wierzymy, że modlitwa jest kluczowym aspektem życia, poprzez modlitwę kontaktujemy się z Bogiem i w ten sposób mamy wpływ na środowisko wokół nas. Modlimy się w 3 aspektach, po pierwsze o osobiste potrzeby osób uczestniczących w spotkaniu oraz tych, które wypełniły kartę modlitwy. Modlimy się o sprawy bieżące oraz błogosławimy nasz kraj, nasze miasto i nasz kościół. Spotkania modlitewne organizowane przez nasz kościół są otwarte i każdy może w nich uczestniczyć."
   },
   {
     name: "English speaking group",
@@ -18,23 +36,12 @@ const groups = [
     description: "The group is the time to read the Bible, pray and build relationships with each other. It is also an opportunity to get to know different cultures and practice English. All foreigners are welcome!"
   },
   {
-    name: "Grupa u Klaudii i Michała",
-    time: "śr 19:00",
-    place: "ul. Litewska (Psie Pole)",
-    description: "Miejsce, gdzie w zabieganej codzienności możesz się bezpiecznie zatrzymać i spędzić czas w rodzinnej atmosferze. Przy dobrym jedzonku dzielimy się tym, co przeżywamy, wzajemnie zachęcamy i spędzamy czas na wspólnej modlitwie. Jeśli masz pragnienie uczyć się praktycznych rzeczy w życiu z Bogiem na podstawie Słowa Bożego, ta grupa jest dla Ciebie!"
-  },
-  {
     name: "Grupa wsparcia",
-    time: "śr 19:00",
+    time: "pt 18:00",
     place: "ul. Klasztorna (Wojszyce)",
     description: "Grupa Dawida, czyli od zera do bohatera. Jeśli potrzebujesz uzdrowienia wewnętrznego, szukasz sensu życia lub potrzebujesz przyjaciół, to miejsce jest dla Ciebie. Na grupie pracujemy nad tym, kim jesteśmy i co mamy w Bogu, jaka jest nasza tożsamość w Chrystusie."
   },
-  {
-    name: "Grupa studencka",
-    time: "śr 19:00",
-    place: "ul. Cybulskiego (Nadodrze)",
-    description: "Wrocław to miasto pełne studentów, a my chcemy ich lepiej poznać! Spotykamy się każdego tygodnia, żeby wspólnie odpocząć, pomodlić się i porozmawiać o ważnych tematach. Dołącz do nas i nie przegap środowych wieczorów, bo razem z Tobą i wieloma wspaniałymi ludźmi chcemy uczyć się, co to znaczy żyć z Bogiem każdego dnia."
-  },
+  
   {
     name: "Grupa młodzieżowa",
     time: "pt 18:00 (co dwa tyg)",
@@ -48,17 +55,17 @@ const groups = [
     description: "Grupa dla wszystkich dorosłych, którzy nadal czują się młodzi. Jeśli się z tym utożsamiasz, to niezależnie czy jesteś studentem, czy już pracujesz, czuj się zaproszony. Na każdym spotkaniu spędzamy czas na pogłębianiu historii biblijnych, dzieleniu się wiarą i dyskusji na aktualne dla nas tematy. A to wszystko przy dobrym jedzeniu i w wesołej atmosferze."
   },
   {
-    name: "Grupa męska",
-    time: "sob 18:00 (pierwsza sob miesiąca)",
-    place: "Kościół Kierunek",
-    description: "Grupa męska to miejsce wzrostu poprzez relację i działanie. W zabieganym świecie pracy i ciągłych wyzwań chcemy zatrzymać się i odpocząć, znaleźć czas, by rozmawiać i rozwijać wartości, które sprawiają, że jesteśmy lepszymi mężczyznami dla siebie, swoich żon i dzieci. Gwarantujemy ciekawie spędzony czas, wartościowe relacje i interesujące tematy rozmów."
-  },
-  {
-    name: "Grupa u Bożenki i Adama",
+    name: "Grupa u Bożenki",
     time: "sob 18:00",
     place: "ul. Jutrzenki (Oporów)",
     description: "Grupa jest otwarta  dla wszystkich: młodych, starszych, małżeństw i singli. Na grupie szukamy Bożej obecności i budujemy relacje. To czas dzielenia się Słowem Bożym, składania świadectw i uwielbienia."
   },
+  {
+    name: "Grupa męska",
+    time: "sob (pierwsza sob miesiąca)",
+    place: "Kościół Kierunek",
+    description: "Grupa męska to miejsce wzrostu poprzez relację i działanie. W zabieganym świecie pracy i ciągłych wyzwań chcemy zatrzymać się i odpocząć, znaleźć czas, by rozmawiać i rozwijać wartości, które sprawiają, że jesteśmy lepszymi mężczyznami dla siebie, swoich żon i dzieci. Gwarantujemy ciekawie spędzony czas, wartościowe relacje i interesujące tematy rozmów."
+  }
 ]
 
 export default groups;

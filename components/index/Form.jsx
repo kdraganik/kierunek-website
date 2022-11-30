b0ba205efe483d5f927780dedd36719fea2bc329
kdraganik <karol.draganik@gmail.com>
2022-11-30 10:05:33 +0100

    Connect contact form to AirTables

--- components/index/Form.jsx
+++ components/index/Form.jsx
@@ -3,15 +3,38 @@ import styles from '../../styles/Form.module.scss'
 
 export default function Form() {
 
+  const [isLoading, setIsLoading] = useState(false)
+  const [status, setStatus] = useState('');
   const [name, setName] = useState("")
   const [email, setEmail] = useState("")
   const [message, setMessage] = useState("")
 
   function handleSubmit(e){
     e.preventDefault();
-    console.log(name)
-    console.log(email)
-    console.log(message)
+    setIsLoading(true);
+    setStatus('');
+    const data = {
+      name,
+      email, 
+      message
+    }
+    fetch("/api/kontakt", {
+      method: "POST",
+      body: JSON.stringify(data)
+    }).then(res =>{
+      if(res.ok){
+        setIsLoading(false);
+        setStatus('SUC')
+        setTimeout(() => setStatus(""), 5000);
+      }
+      else{
+        throw new Error("Bad request")
+      }
+    }).catch(err => {
+      console.log(err);
+      setIsLoading(false);
+      setStatus('ERR')
+    })
   }
 
   return(
@@ -23,7 +46,9 @@ export default function Form() {
       <input required type="email" className={styles.textInput} id="email" value={email} onChange={e => setEmail(e.target.value)}/>
       <label htmlFor="message" className={styles.label}>Wiadomość</label>
       <input required type="message" className={styles.textInput} id="message" value={message} onChange={e => setMessage(e.target.value)}/>
-      <input className={styles.submit} type="submit" value="wyślij"/>
+      {status == 'SUC' ? <div className={styles.success}>Dziękujemy za kontakt, wkrótce się odezwiemy!</div> : ""}
+      {status == 'ERR' ? <div className={styles.error}>Wystąpił błąd, spróbuj ponownie</div> : ""}
+      {isLoading ? <div className={styles.loader}><div></div><div></div><div></div><div></div></div> : <input type="submit" className={styles.submit} value="Wyślij" />}
     </form>
   )
 }
---
+++ pages/api/kontakt.js
@@ -0,0 +1,40 @@
+export default async function handler(req, res){
+    const apiKey = process.env.AIRTABLE_API_KEY;
+
+    const data = JSON.parse(req.body)
+
+    const bodyData = {
+        "records": [
+            {
+                "fields": {
+                    "Imię": data.name,
+                    "Email": data.email,
+                    "Wiadomość": data.message
+                }
+            }
+        ]
+    }
+
+    try{
+        const response = await fetch("https://api.airtable.com/v0/appsCFczCvOsdxKRE/Lista", {
+            method: 'POST',
+            headers: {
+                "Authorization": "Bearer " + apiKey,
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(bodyData)
+        })
+        if(response.ok){
+            res.status(200).json({message: "ok"})
+        }
+        else{
+            const data = await response.json()
+            console.log(data)
+            res.status(502).json({message: "Error while saving data to AirTables"})
+        }
+    }
+    catch(err){
+        console.log(err)
+        res.status(502).json({message: "Error while saving data to AirTables"})
+    }  
+}
